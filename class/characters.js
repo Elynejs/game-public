7cdab2958c32bc7dadfbd1338c5391d58bd4bba7
Elynejs <matthias.gomes03@gmail.com>
2019-08-14 21:14:45 +0200

    Create characters.js

---
+++ class/characters.js
@@ -0,0 +1,16 @@
+class Characters {
+    constructor(
+        //
+    ) {
+        //
+    }
+}
+
+class S extends Characters {
+    constructor(
+        //
+    ) {
+        super();
+        //
+    }
+}
